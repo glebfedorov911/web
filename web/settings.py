99e022df618a71b4af63c25a18c1242c346df1d9
Gleb <fedorov22134@gmail.com>
2024-11-27 17:19:28 +0300

    logs

--- web/settings.py
+++ web/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
 
+print("ZALUPA KONYA", BASE_DIR)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
